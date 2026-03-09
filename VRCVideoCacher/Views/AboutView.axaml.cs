3797b7fc8e044900f1b14fc274fc97f3ef7704ba
DubyaDude <ushafiq141@gmail.com>
2026-03-08 23:05:02 -0400

    Add links

--- VRCVideoCacher/Views/AboutView.axaml.cs
+++ VRCVideoCacher/Views/AboutView.axaml.cs
@@ -9,4 +9,33 @@ public partial class AboutView : UserControl
         InitializeComponent();
         DataContext = new VRCVideoCacher.ViewModels.AboutViewModel();
     }
+    
+    private void OnDiscordClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        OpenUrl("https://discord.gg/t6x6p6Tzs");
+    }
+    
+    private void OnGitHubClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        OpenUrl("https://github.com/EllyVR/VRCVideoCacher");
+    }
+    
+    private void OnSteamClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        OpenUrl("https://store.steampowered.com/app/4296960/VRCVideoCacher/");
+    }
+    
+    private void OpenUrl(string url)
+    {
+        try
+        {
+            var psi = new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = url,
+                UseShellExecute = true
+            };
+            System.Diagnostics.Process.Start(psi);
+        }
+        catch { /* Optionally handle errors */ }
+    }
 }
