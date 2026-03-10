cedeaa2d1418d9e4d6e4c43b2857239718c0ce57
DubyaDude <ushafiq141@gmail.com>
2026-03-10 00:41:03 -0400

    Remove legacy config migration

--- VRCVideoCacher/ConfigManager.cs
+++ VRCVideoCacher/ConfigManager.cs
@@ -29,8 +29,6 @@ public class ConfigManager
         {
             if (File.Exists(ConfigFilePath))
                 newConfig = JsonConvert.DeserializeObject<ConfigModel>(File.ReadAllText(ConfigFilePath));
-            if (newConfig == null)
-                newConfig = MigrateLegacyConfig();
             if (newConfig != null)
                 Config = newConfig;
         }
@@ -61,94 +59,6 @@ public class ConfigManager
         TrySaveConfig();
     }
     
-    private static ConfigModel? MigrateLegacyConfig()
-    {
-        if (!OperatingSystem.IsWindows())
-            return null;
-
-        try
-        {
-            // migrate legacy config if it exists
-            var legacyConfigPath = Path.Join(Program.CurrentProcessPath, "config.json");
-            if (!File.Exists(legacyConfigPath))
-                return null;
-
-            var legacyConfig = JsonConvert.DeserializeObject<LegacyConfigModel>(File.ReadAllText(legacyConfigPath));
-            if (legacyConfig == null)
-                return null;
-            
-            // move our files and folders
-            var folderList = new List<string>
-            {
-                "Utils",
-                "MetadataCache"
-            };
-            var fileList = new List<string>
-            {
-                "version.json",
-                "yt-dlp.version.txt",
-                "youtube_cookies.txt"
-            };
-            foreach (var folder in folderList)
-            {
-                var oldPath = Path.Join(Program.CurrentProcessPath, folder);
-                var newPath = Path.Join(Program.DataPath, folder);
-                if (Directory.Exists(oldPath))
-                {
-                    if (Directory.Exists(newPath))
-                        Directory.Delete(newPath, true);
-                    Directory.Move(oldPath, newPath);
-                }
-            }
-            foreach (var file in fileList)
-            {
-                var oldPath = Path.Join(Program.CurrentProcessPath, file);
-                var newPath = Path.Join(Program.DataPath, file);
-                if (File.Exists(oldPath))
-                {
-                    if (File.Exists(newPath))
-                        File.Delete(newPath);
-                    File.Move(oldPath, newPath);
-                }
-            }
-
-            // Use existing cache directory if it has cached assets
-            var oldCachePath = Path.Join(Program.CurrentProcessPath, "CachedAssets");
-            var isCacheDirInUse = Directory.Exists(oldCachePath) && Directory.GetFiles(oldCachePath).Length > 1;
-            var newCachePath = isCacheDirInUse ? oldCachePath : legacyConfig.CachedAssetPath;
-
-            File.Delete(legacyConfigPath);
-            Log.Information("Migrated legacy config from {LegacyConfigPath}", legacyConfigPath);
-            return new ConfigModel
-            {
-                YtdlpWebServerUrl = legacyConfig.ytdlWebServerURL,
-                YtdlpGlobalPath = string.IsNullOrEmpty(legacyConfig.ytdlPath),
-                YtdlpUseCookies = legacyConfig.ytdlUseCookies,
-                YtdlpAutoUpdate = legacyConfig.ytdlAutoUpdate,
-                YtdlpAdditionalArgs = legacyConfig.ytdlAdditionalArgs,
-                YtdlpDubLanguage = legacyConfig.ytdlDubLanguage,
-                CachedAssetPath = newCachePath,
-                BlockedUrls = legacyConfig.BlockedUrls,
-                BlockRedirect = legacyConfig.BlockRedirect,
-                CacheYouTube = legacyConfig.CacheYouTube,
-                CacheYouTubeMaxResolution = legacyConfig.CacheYouTubeMaxResolution,
-                CacheYouTubeMaxLength = legacyConfig.CacheYouTubeMaxLength,
-                CacheMaxSizeInGb = legacyConfig.CacheMaxSizeInGb,
-                CachePyPyDance = legacyConfig.CachePyPyDance,
-                CacheVrDancing = legacyConfig.CacheVRDancing,
-                PatchResonite = legacyConfig.PatchResonite,
-                ResonitePath = legacyConfig.ResonitePath,
-                PatchVrChat = legacyConfig.PatchVRC,
-                AutoUpdateVrcVideoCacher = legacyConfig.AutoUpdate
-            };
-        }
-        catch (Exception ex)
-        {
-            Log.Error(ex, "Failed to migrate legacy config, it may be corrupted. Recreating...");
-            return null;
-        }
-    }
-
     public static void TrySaveConfig()
     {
         var newConfig = JsonConvert.SerializeObject(Config, Formatting.Indented);
@@ -261,33 +171,3 @@ public class ConfigModel
     // Localization
     public string Language = "en";
 }
-
-
-
-// ReSharper disable InconsistentNaming
-public class LegacyConfigModel
-{
-    public string ytdlWebServerURL = "http://localhost:9696";
-    public string ytdlPath = OperatingSystem.IsWindows() ? "Utils\\yt-dlp.exe" : Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "VRCVideoCacher/Utils/yt-dlp");
-    public bool ytdlUseCookies = true;
-    public bool ytdlAutoUpdate = true;
-    public string ytdlAdditionalArgs = string.Empty;
-    public string ytdlDubLanguage = string.Empty;
-    public int ytdlDelay = 0;
-    public string CachedAssetPath = "";
-    public string[] BlockedUrls = ["https://na2.vrdancing.club/sampleurl.mp4"];
-    public string BlockRedirect = "https://www.youtube.com/watch?v=byv2bKekeWQ";
-    public bool CacheYouTube = false;
-    public int CacheYouTubeMaxResolution = 1080;
-    public int CacheYouTubeMaxLength = 120;
-    public float CacheMaxSizeInGb = 0;
-    public bool CachePyPyDance = false;
-    public bool CacheVRDancing = false;
-    public bool PatchResonite = false;
-    public string ResonitePath = "";
-    public bool PatchVRC = true;
-    public bool AutoUpdate = true;
-    public string[] PreCacheUrls = [];
-    
-}
-// ReSharper restore InconsistentNaming
