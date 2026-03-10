8c4cb0797f991b23bf5a079567fa44ce07a3e07d
DubyaDude <ushafiq141@gmail.com>
2026-03-09 21:14:55 -0400

    Create hosts file if does not exist

--- VRCVideoCacher/Utils/HostsManager.cs
+++ VRCVideoCacher/Utils/HostsManager.cs
@@ -12,6 +12,7 @@ public class HostsManager
     
     public static void TryRun()
     {
+        CreateHostsIfNotExists();
         if (Environment.CommandLine.Contains("--addhost"))
         {
             try
@@ -66,7 +67,41 @@ public class HostsManager
     
     public static bool IsHostAdded()
     {
+        CreateHostsIfNotExists();
         var hostsFile = File.ReadAllText(HostsPath);
         return hostsFile.Contains(Header);
     }
+
+    private static void CreateHostsIfNotExists()
+    {
+        if (!File.Exists(HostsPath))
+        {
+            Log.Information("Hosts file not found at {HostsPath}. Creating a new one with default content.", HostsPath);
+            File.WriteAllText(HostsPath, DefaultHostsFile);
+        }
+    }
+
+    // Default content for the hosts file, based on the standard Windows hosts file.
+    // Source: https://support.microsoft.com/en-us/topic/how-to-reset-the-hosts-file-back-to-the-default-c2a43f9d-e176-c6f3-e4ef-3500277a6dae
+    private const string DefaultHostsFile = @"# Copyright (c) 1993-2009 Microsoft Corp.
+#
+# This is a sample HOSTS file used by Microsoft TCP/IP for Windows.
+#
+# This file contains the mappings of IP addresses to host names. Each
+# entry should be kept on an individual line. The IP address should
+# be placed in the first column followed by the corresponding host name.
+# The IP address and the host name should be separated by at least one
+# space.
+#
+# Additionally, comments (such as these) may be inserted on individual
+# lines or following the machine name denoted by a '#' symbol.
+#
+# For example:
+#
+#      102.54.94.97     rhino.acme.com          # source server
+#       38.25.63.10     x.acme.com              # x client host
+# localhost name resolution is handled within DNS itself.
+#    127.0.0.1       localhost
+#    ::1             localhost
+";
 }
